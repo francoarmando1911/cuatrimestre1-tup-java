0328af935c73d53f7f702c9b8346a06b5cceafec
Franco Armando <francoarmando1911@gmail.com>
2024-11-25 17:46:10 -0300

    ejercicio2 for

--- src/main/java/guia10For/ejercicio2.java
+++ src/main/java/guia10For/ejercicio2.java
@@ -3,9 +3,24 @@ package guia10For;
 // Escriba un pseudocódigo que muestre por pantalla un árbol de caracteres asteriscos (*) .
 // Permita ingresar la cantidad de niveles (profundidad) del árbol.
 
+import java.util.Scanner;
+
 public class ejercicio2 {
-    public int arbol(int nivel){
+    public void arbol(int nivel){
+        for(int i=1; i<= nivel; i++){
+            String espacios = "".repeat(nivel-i);
+            String asteriscos = "*".repeat(2 * i -1);
+            System.out.println(espacios + asteriscos);
+        }
+    }
 
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        System.out.println("Ingrese la cantidad de niveles que desea que tenga el arbol: ");
+        int nivel = entrada.nextInt();
 
-    };
+        ejercicio2 arbol = new ejercicio2(); //creo una instancia de la funcion (cosas de java....)
+        arbol.arbol(nivel);
+
+    }
 }
