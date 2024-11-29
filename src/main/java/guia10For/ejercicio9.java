ace091604c42a1e0a557a6b6804cb29dc9300db7
Franco Armando <francoarmando1911@gmail.com>
2024-11-29 17:22:36 -0300

    ejercicio 9 contadorVocales - for

--- src/main/java/guia10For/ejercicio9.java
+++ src/main/java/guia10For/ejercicio9.java
@@ -5,10 +5,24 @@ Crea un pseudocódigo que reciba una palabra ingresada por el usuario y cuente c
 contiene. Considere que la palabra se ingresa en minúsculas completamente.
 */
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class ejercicio9 {
     public static void main(String[] args) {
         Scanner entrada = new Scanner(System.in);
+
+        System.out.println("Ingrese una palabra en minuscula: ");
+        String palabra = entrada.next();
+        int contadorVocales;
+
+        for(int i = 0; i < palabra.length(); i++){
+            String letra = palabra[i];
+            if(letra == 'a' or letra == 'e' or letra == "i" or letra == "o" or letra == "u"){
+                contadorVocales++;
+            }
+        }
+
+        System.out.println("La palabra tiene: " + contadorVocales + " vocales.");
     }
 }
