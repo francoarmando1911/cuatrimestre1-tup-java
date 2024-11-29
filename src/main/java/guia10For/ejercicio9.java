5e4064f6de7f913e566eac19aa88b15f42750849
Franco Armando <francoarmando1911@gmail.com>
2024-11-29 11:08:53 -0300

    actualizacion

--- src/main/java/guia10For/ejercicio9.java
+++ src/main/java/guia10For/ejercicio9.java
@@ -1,4 +1,14 @@
 package guia10For;
 
+/*
+Crea un pseudocódigo que reciba una palabra ingresada por el usuario y cuente cuántas vocales
+contiene. Considere que la palabra se ingresa en minúsculas completamente.
+*/
+
+import java.util.Scanner;
+
 public class ejercicio9 {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+    }
 }
