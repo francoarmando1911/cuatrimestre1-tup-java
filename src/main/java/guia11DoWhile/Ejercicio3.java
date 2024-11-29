45a3617ea6729dbf3312d973b8c8bbbabef23810
Franco Armando <francoarmando1911@gmail.com>
2024-11-29 18:27:40 -0300

    inicializacion ejercicio3

---
+++ src/main/java/guia11DoWhile/Ejercicio3.java
@@ -0,0 +1,7 @@
+package guia11DoWhile;
+//Escribir un pseudocódigo que permita al usuario adivinar un número secreto entre 1 y
+//10. El programa sigue pidiendo intentos hasta que el número sea adivinado
+
+//Math.random para el numero secreto
+public class Ejercicio3 {
+}
