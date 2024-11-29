f53aad9b84e93ef634d212b8103100ba67823651
Franco Armando <francoarmando1911@gmail.com>
2024-11-29 17:33:50 -0300

    ejercicio1 - dowhile

--- src/main/java/guia11DoWhile/ejercicio1.java
+++ src/main/java/guia11DoWhile/ejercicio1.java
@@ -1,7 +1,22 @@
 package guia11DoWhile;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
 // Crear un pseudocódigo que le permita ingresar todos los números que desee y calcular u sumatoria. Establezca una opción de salida.
 public class ejercicio1 {
+ public static void main(String[] args) {
+  Scanner entrada = new Scanner(System.in);
 
+  ArrayList<Integer> numeros = new ArrayList<>(); //Lista de numeros
+  int sumaTotal = 0;
+  char respuesta;
 
+  System.out.println("Ingrese un numero para sumar o escriba 0 para salir: ");
+  int numero = entrada.nextInt();
+
+  if (numero == 0) break;
 
  }
+
+}
