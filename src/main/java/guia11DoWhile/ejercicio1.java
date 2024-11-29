38389a184855bb14b79778c8700f8512edd17e20
Franco Armando <francoarmando1911@gmail.com>
2024-11-29 17:27:44 -0300

    ejercicio1 - guia 11 DoWhile

--- src/main/java/guia11DoWhile/ejercicio1.java
+++ src/main/java/guia11DoWhile/ejercicio1.java
@@ -1,5 +1,5 @@
 package guia11DoWhile;
-
+// Crear un pseudocódigo que le permita ingresar todos los números que desee y calcular u sumatoria. Establezca una opción de salida.
 public class ejercicio1 {
 
 
