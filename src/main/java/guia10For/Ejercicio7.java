4993fbd80921181206ff5bba6cc201c063a33581
Franco Armando <francoarmando1911@gmail.com>
2024-11-28 21:39:03 -0300

    ejercicio7 - for

---
+++ src/main/java/guia10For/Ejercicio7.java
@@ -0,0 +1,18 @@
+package guia10For;
+
+/*
+Cree un pseudocódigo que permita al usuario ingresar 6 números enteros, que pueden ser
+positivos o negativos. Al finalizar, mostrar la sumatoria de los números negativos y el promedio de
+los positivos.
+*/
+
+public class Ejercicio7 {
+
+    int sumaNegativos = 0;
+    int sumaPositivos = 0;
+    int contadorPositivos = 0;
+
+    for(int i = 0; i <= 6; i++){
+
+    }
+}
