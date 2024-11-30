2902a40fc8671190a8b93a9d6afa0293dc1b1c69
Franco Armando <francoarmando1911@gmail.com>
2024-11-30 09:47:43 -0300

    inicializacion ejercicio4

---
+++ src/main/java/guia11DoWhile/Ejercicio4.java
@@ -0,0 +1,19 @@
+package guia11DoWhile;
+
+import java.util.Scanner;
+
+/*
+En una caja de supermercado se ingresa una serie de productos escriba un
+pseudocódigo que calcule cuanto es el total de la compra. Debido a que hay promociones
+se hace un descuento de 10 % a los productos Lácteos y 5 % de descuento a los
+productos Cárnicos. Se debe ingresar cantidad, precio y tipo. La finalización del ingreso de
+productos se produce cuando se ingresa precio 0.
+*/
+public class Ejercicio4 {
+
+
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+
+    }
+}
