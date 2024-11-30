392a1a6b4c98fadd03684b5bb1bf11b93298a4ec
Franco Armando <francoarmando1911@gmail.com>
2024-11-30 10:38:18 -0300

    inicializacion 5

---
+++ src/main/java/guia11DoWhile/Ejercicio5.java
@@ -0,0 +1,10 @@
+package guia11DoWhile;
+/*
+Escriba un pseudocódigo que permita ingresar palabras y que nos informe cuantas
+palabras ingreso y muestre cual de todas fue la más larga y cuantas letras tenía esa
+palabra. Establezca una opción para finalizar el ingreso de palabras. No considere la
+palabra que utiliza para finalizar para el conteo de palabras ni ningún otro requerimiento.
+*/
+
+public class Ejercicio5 {
+}
