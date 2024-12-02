07ae2728b0928e97a8696e699711be70dcccf4a9
Franco Armando <francoarmando1911@gmail.com>
2024-12-01 21:26:13 -0300

    inicializacion ejercicio 6

---
+++ src/main/java/guia11DoWhile/ejercicio6.java
@@ -0,0 +1,17 @@
+package guia11DoWhile;
+
+import java.util.Scanner;
+
+/*
+Escriba un pseudocódigo que permita el ingreso de una serie de números y que a su
+sumatoria le calcule cuantas cifras tiene y que muestre cada una de sus cifras por
+separado. Establezca una opción para la finalización del ingreso de la serie.
+8- Escriba un pseudocódigo que permita el ingreso de una frase y que cuente la cantidad
+de palabras que el usuario que ingreso. Establezca una opción para finalizar el ingreso de
+palabras.
+*/
+public class ejercicio6 {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+    }
+}
