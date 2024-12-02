303acacfa8d01200279bf281cda45675d45ec338
Franco Armando <francoarmando1911@gmail.com>
2024-12-02 17:04:33 -0300

    inicializacion ejercicio 9

--- src/main/java/guia11DoWhile/Ejercicio9.java
+++ src/main/java/guia11DoWhile/Ejercicio9.java
@@ -1,8 +1,14 @@
 package guia11DoWhile;
+
+import java.util.Scanner;
+
 /*
 Escriba un pseudocódigo que permita el ingreso de un número de palabras para cada palabra informe cuantas letras vocales tiene
 y cuantas palabras ingresó. Además considere que puede ingresar mayúsculas y minúsculas. Si necesita usar estructuras cíclicas
 utilice solamente Ciclo Repetir Hasta.
  */
 public class Ejercicio9 {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+    }
 }
