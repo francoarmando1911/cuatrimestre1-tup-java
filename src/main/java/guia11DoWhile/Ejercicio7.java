9b02a64220026bafcdbc7c043a9a8948f3f7473a
Franco Armando <francoarmando1911@gmail.com>
2024-12-02 17:38:21 -0300

    ejercicio7 - contador de palabras de frase

--- src/main/java/guia11DoWhile/Ejercicio7.java
+++ src/main/java/guia11DoWhile/Ejercicio7.java
@@ -15,5 +15,28 @@ usuario que ingreso. Establezca una opción para finalizar el ingreso de palabra
 public class Ejercicio7 {
     public static void main(String[] args) {
         Scanner entrada = new Scanner(System.in);
+
+        while (true){
+            System.out.println("Ingrese una opcion: 1 - ingresar / 2 - salir");
+            int opc = entrada.nextInt();
+            entrada.nextLine();
+
+            if(opc == 2){
+                System.out.println("Saliendo...");
+                break;
+
+            } else if (opc == 1) {
+                System.out.println("Ingrese una frase: ");
+                String frase = entrada.nextLine();
+
+                String[] palabras = frase.trim().split("\\s+");
+
+                int contadorPalabras = palabras.length;
+
+                System.out.println("La cantidad de palabras que ingreso en su frase es: " + contadorPalabras);
+            } else {
+                System.out.println("Opcion no valida, intente nuevamente");
+            }
+        }
     }
 }
