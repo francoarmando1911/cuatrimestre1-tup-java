06f15161cfbfc792e33f3bdf97952109b537947a
Franco Armando <francoarmando1911@gmail.com>
2024-12-02 17:19:54 -0300

    ejercicio7

--- src/main/java/guia11DoWhile/Ejercicio7.java
+++ src/main/java/guia11DoWhile/Ejercicio7.java
@@ -5,6 +5,12 @@ import java.util.Scanner;
 /*
 Escriba un pseudocódigo que permita el ingreso de una frase y que cuente la cantidad de palabras que el
 usuario que ingreso. Establezca una opción para finalizar el ingreso de palabras.
+
+---APUNTES---
+- Ingreso de frase
+- Dividir la frase en palabras, utilizando el espacio como delimitador
+    -String[] palabras = frases.trim().split(\\s+)
+- Contad la cantidad de palabras con cantPalabras = palabras.length()
 */
 public class Ejercicio7 {
     public static void main(String[] args) {
