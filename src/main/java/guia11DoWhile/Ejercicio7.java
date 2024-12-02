4822445aab30961fdc6d36b01944a35e057c8740
Franco Armando <francoarmando1911@gmail.com>
2024-12-02 17:03:50 -0300

    inicializacion ejercicio8

--- src/main/java/guia11DoWhile/Ejercicio7.java
+++ src/main/java/guia11DoWhile/Ejercicio7.java
@@ -1,7 +1,13 @@
 package guia11DoWhile;
+
+import java.util.Scanner;
+
 /*
 Escriba un pseudocódigo que permita el ingreso de una frase y que cuente la cantidad de palabras que el
 usuario que ingreso. Establezca una opción para finalizar el ingreso de palabras.
 */
 public class Ejercicio7 {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+    }
 }
--- src/main/java/guia11DoWhile/Ejercicio8.java
+++ src/main/java/guia11DoWhile/Ejercicio8.java
@@ -1,9 +1,15 @@
 package guia11DoWhile;
 
+import java.util.Scanner;
+
 /**
  Cree un pseudocódigo que registre los resultados de arrojar un dado y nos informe en que intento salió el 4.
  El pseudocódigo debe permitir repetir las partidas. (Se considera partida la secuencia de arrojar los
  dados hasta que salga el 4 o hasta que ingrese 0). La opción para terminar el juego  es la opción resultado 0.
  */
 public class Ejercicio8 {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+
+    }
 }
