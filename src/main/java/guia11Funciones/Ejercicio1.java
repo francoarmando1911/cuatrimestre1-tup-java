9a05fe82a3a339f7b5859d13e643f0f643b148f2
Franco Armando <francoarmando1911@gmail.com>
2024-12-02 20:39:11 -0300

    inicializacion ejercicio1 - guia11 funciones

---
+++ src/main/java/guia11Funciones/Ejercicio1.java
@@ -0,0 +1,11 @@
+package guia11Funciones;
+/*
+Escriba un menú que contenga las siguientes opciones y programe el pseudocódigo que efectué la acción indicada. Utilice una función para cada ítem del menú.
+1-Multiplicacion de dos números
+2-Suma de dos números
+3-Resta de dos numero
+4-Salir
+
+ */
+public class Ejercicio1 {
+}
