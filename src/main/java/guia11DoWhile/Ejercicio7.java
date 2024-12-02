7fb25e9f13e547814e3bdda1904c2131f4587262
Franco Armando <francoarmando1911@gmail.com>
2024-12-02 17:00:52 -0300

    actualizacion

---
+++ src/main/java/guia11DoWhile/Ejercicio7.java
@@ -0,0 +1,7 @@
+package guia11DoWhile;
+/*
+Escriba un pseudocódigo que permita el ingreso de una frase y que cuente la cantidad de palabras que el
+usuario que ingreso. Establezca una opción para finalizar el ingreso de palabras.
+*/
+public class Ejercicio7 {
+}
