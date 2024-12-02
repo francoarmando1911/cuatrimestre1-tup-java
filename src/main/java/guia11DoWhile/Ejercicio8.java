f82e12777603b3d5d9a556e9c0927f0fe8c00808
Franco Armando <francoarmando1911@gmail.com>
2024-12-02 17:02:15 -0300

    inicializaciones

--- src/main/java/guia11DoWhile/Ejercicio8.java
+++ src/main/java/guia11DoWhile/Ejercicio8.java
@@ -1,7 +1,9 @@
 package guia11DoWhile;
 
 /**
-
+ Cree un pseudocódigo que registre los resultados de arrojar un dado y nos informe en que intento salió el 4.
+ El pseudocódigo debe permitir repetir las partidas. (Se considera partida la secuencia de arrojar los
+ dados hasta que salga el 4 o hasta que ingrese 0). La opción para terminar el juego  es la opción resultado 0.
  */
 public class Ejercicio8 {
 }
---
+++ src/main/java/guia11DoWhile/Ejercicio9.java
@@ -0,0 +1,8 @@
+package guia11DoWhile;
+/*
+Escriba un pseudocódigo que permita el ingreso de un número de palabras para cada palabra informe cuantas letras vocales tiene
+y cuantas palabras ingresó. Además considere que puede ingresar mayúsculas y minúsculas. Si necesita usar estructuras cíclicas
+utilice solamente Ciclo Repetir Hasta.
+ */
+public class Ejercicio9 {
+}
