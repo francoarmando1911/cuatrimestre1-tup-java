b47dbc2aee131bd1e042e3f4c47d02ce582f0e1e
Franco Armando <francoarmando1911@gmail.com>
2024-12-02 22:35:02 -0300

    ejercicio3 inicializacion

--- src/main/java/guia11Funciones/Ejercicio2.java
+++ src/main/java/guia11Funciones/Ejercicio2.java
@@ -1,4 +1,8 @@
 package guia11Funciones;
-
+/*
+Crear  un pseudocódigo que  permita que, ingresando el nombre y el sexo de una persona nos muestre un saludo de
+buenos días personalizado con ese nombre. Cree una función para que si es mujer agregue “Sra” y si es hombre “Sr.”
+delante del nombre ingresado.
+ */
 public class Ejercicio2 {
 }
---
+++ src/main/java/guia11Funciones/Ejercicio3.java
@@ -0,0 +1,8 @@
+package guia11Funciones;
+/*
+Escribir  un algoritmo que nos informe si el año ingresado es un año bisiesto.
+Utilice una función para determinarlo.
+ */
+public class Ejercicio3 {
+
+}
