18ae312e6a3e5011c54b45aeb0c02edbdb8f8a5a
Franco Armando <francoarmando1911@gmail.com>
2024-12-02 22:36:26 -0300

    inicializacion ejercicio4

---
+++ src/main/java/guia11Funciones/Ejercicio4.java
@@ -0,0 +1,9 @@
+package guia11Funciones;
+/*
+En una estación de servicio se aplica un 5% de descuento por litro si la compra de nafta Premium es de más de
+100 litros, pero si la compra es de más de 1000 litros se le agrega un 3% de descuento adicional. Cree un algoritmo
+que calcule el monto total de una compra ingresando la cantidad de litros y teniendo en cuenta que el precio original
+de la nafta Premium es de $1500. Utilice una función para calcular el precio final por litro de la compra.
+ */
+public class Ejercicio4 {
+}
