8dffaad42185188e94b881a1b10cd1fdb33e2bb4
Franco Armando <francoarmando1911@gmail.com>
2024-12-03 08:12:14 -0300

    inicializacion ejercicio3

--- src/main/java/guia11Funciones/Ejercicio3.java
+++ src/main/java/guia11Funciones/Ejercicio3.java
@@ -1,8 +1,28 @@
 package guia11Funciones;
+
+import java.util.Scanner;
+
 /*
 Escribir  un algoritmo que nos informe si el año ingresado es un año bisiesto.
 Utilice una función para determinarlo.
+
+--APUNTES--
+
+- Crear funcion boolean anioBisiesto que tome como parametro la variable anio
+- si ((anio % 4 == 0 && anio % 100 != 0) || (anio % 400 == 0))
+        return True
+    else:
+        return False
  */
 public class Ejercicio3 {
+
+
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+
+        System.out.println("Ingrese un año: ");
+        int anio = entrada.nextInt();
+
+    }
 
 }
