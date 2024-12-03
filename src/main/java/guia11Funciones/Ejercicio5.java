c61c6f1aae0780c44defec1aa2ba2445b390904b
Franco Armando <francoarmando1911@gmail.com>
2024-12-02 22:38:37 -0300

    inicializacion ejercicio5

---
+++ src/main/java/guia11Funciones/Ejercicio5.java
@@ -0,0 +1,10 @@
+package guia11Funciones;
+/*
+En una estación de servicio deciden dar como premio un 5% de descuento a todas las compras que se realicen por monto
+de dinero (no por litro) y que la cantidad de litros resultante de esa compra sea un número par. Por ejemplo si una
+persona pide $12000 y el surtidor marca 8.33 litros se considera que es par (se desprecia la parte decimal).
+Escriba un algoritmo que calcule el total de la compra utilizando una función que calcule si el número es par o impar.
+ */
+public class Ejercicio5 {
+
+}
