87ac9daf9ac40804b67f130cf08d82b7e3ac799e
Franco Armando <francoarmando1911@gmail.com>
2024-12-04 09:56:11 -0300

    inicializacion ejercicio10 - funciones

--- src/main/java/guia11Funciones/Ejercicio10.java
+++ src/main/java/guia11Funciones/Ejercicio10.java
@@ -1,6 +1,9 @@
 package guia11Funciones;
 /*
-
+Cree un pseudocódigo que permita el ingreso de números enteros y nos informe cuantos números pares y cuantos
+impares  ingresó. Cree una función que determine si el número es par o impar y vaya acumulando las cantidades.
+Establezca una opción para finalizar el ingreso de números. Intente validar que el número ingresado sea un entero.
+Utilice parámetros por referencia si es necesario.
  */
 public class Ejercicio10 {
 }
