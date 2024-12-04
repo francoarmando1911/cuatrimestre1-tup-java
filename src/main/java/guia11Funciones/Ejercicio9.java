92ec7c7764ef98d06653ade5990e5195c25a2e40
Franco Armando <francoarmando1911@gmail.com>
2024-12-04 09:55:18 -0300

    inicializacion ejercicio9 - funciones

--- src/main/java/guia11Funciones/Ejercicio9.java
+++ src/main/java/guia11Funciones/Ejercicio9.java
@@ -1,6 +1,11 @@
 package guia11Funciones;
 /*
-
+En una caja de supermercado se ingresa una serie de productos escriba un pseudocódigo que calcule cuanto es el
+total de la compra y cuantos productos Lácteos compró. Debido a que hay promociones se hace un descuento de 10 %
+a los productos Lácteos y 5 % de descuento a los productos Cárnicos. Se debe ingresar cantidad, precio y tipo.
+Utilice una función para calcular las compras individuales, acumular los totales de la compra y la cantidad de Lácteos.
+La finalización del ingreso de productos se produce cuando se ingresa precio 0. Utilice parámetros por
+referencia si es necesario.
  */
 public class Ejercicio9 {
 }
