042015b41ca5206c5a6692ef3f89ad84cf0c09ca
Franco Armando <francoarmando1911@gmail.com>
2024-12-04 09:53:06 -0300

    ejercicio 8 - funciones

--- src/main/java/guia11Funciones/Ejercicio7.java
+++ src/main/java/guia11Funciones/Ejercicio7.java
@@ -1,4 +1,8 @@
 package guia11Funciones;
+/*
+Escriba un pseudocódigo que permita el ingreso de una fecha y determine si es válida o no. Se considera una
+fecha valida si tiene el formato ddmmaaaa . Escriba una función para validad lo ingresado.
+ */
 
 public class Ejercicio7 {
 }
