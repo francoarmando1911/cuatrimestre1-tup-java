fcd05f7207e55dd0394f6ff781266b0581bad10d
Franco Armando <francoarmando1911@gmail.com>
2024-12-09 10:57:37 -0300

    inicializacion ejercicio2 - guia15

---
+++ src/main/java/guia15Arreglos/Ejercicio2.java
@@ -0,0 +1,14 @@
+package guia15Arreglos;
+
+import java.util.Scanner;
+
+/*
+Escriba un algoritmo que cargue 5 números en un arreglo y que encuentre el mayor de ellos.
+ */
+public class Ejercicio2 {
+
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+
+    }
+}
