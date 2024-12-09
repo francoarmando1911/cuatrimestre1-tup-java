d1b3189e881d4a4acaa74a3a5e80d52bad85e46d
Franco Armando <francoarmando1911@gmail.com>
2024-12-09 10:55:56 -0300

    inicializacion ejercicio1 - guia15

--- src/main/java/guia15Arreglos/Ejercicio1.java
+++ src/main/java/guia15Arreglos/Ejercicio1.java
@@ -1,4 +1,16 @@
 package guia15Arreglos;
 
+import java.util.Scanner;
+
+/*
+Escriba un algoritmo que permita la lectura de 5 números y que muestre los números ingresados en orden inverso al
+que se los ingreso. Utilice un arreglo para la solución del algoritmo.
+ */
 public class Ejercicio1 {
+
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+
+
+    }
 }
