f45832f83e5a35da4a158770c3439591953d5be5
Franco Armando <francoarmando1911@gmail.com>
2024-12-09 10:59:05 -0300

    iniciaizacion ejercicio3 - guia15

---
+++ src/main/java/guia15Arreglos/Ejercicio3.java
@@ -0,0 +1,15 @@
+package guia15Arreglos;
+
+import java.util.Scanner;
+
+/*
+Cree un algoritmo que cargue un arreglo de 5 elementos, invierta su contenido (el contendido del último elemento pasa
+a ser el primero y así sucesivamente) y que muestre el contenido invertido.
+ */
+public class Ejercicio3 {
+
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+
+    }
+}
