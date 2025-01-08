fb7dbed638e32d8a4aa7ac8c6e5007698b30fbe1
Franco Armando <francoarmando1911@gmail.com>
2025-01-08 10:03:56 -0300

    inicializacion ejercicio dias que lelva el anio

---
+++ src/main/java/logica/diaAnio.java
@@ -0,0 +1,15 @@
+package logica;
+
+import java.util.Scanner;
+
+/*
+ * Haga una funcion que reciba como entrada un mes del año y determine el numero de
+ * dias transcurridos desde el comienzo del año hasta el primer dia del mes
+ */
+public class diaAnio {
+    
+
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+    }
+}
