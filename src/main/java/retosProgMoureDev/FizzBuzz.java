53d205a783b0e29b2f0433402e0587f6833e88c7
Franco Armando <francoarmando1911@gmail.com>
2025-01-08 08:24:47 -0300

    Actualizacion fizz buzz

--- src/main/java/retosProgMoureDev/FizzBuzz.java
+++ src/main/java/retosProgMoureDev/FizzBuzz.java
@@ -12,8 +12,15 @@ package retosProgMoureDev;
 public class FizzBuzz {
     public static void main(String[] args) {
         for(int index = 1; index <= 100; index++){
-            
-
+            if (index % 3 == 0 && index % 5 == 0) {  
+                System.out.println("fizzbuzz");  
+            } else if (index % 3 == 0) {  
+                System.out.println("fizz");  
+            } else if (index % 5 == 0) {  
+                System.out.println("buzz");  
+            } else {  
+                System.out.println(index);  
+            } 
         }
     }
 }
