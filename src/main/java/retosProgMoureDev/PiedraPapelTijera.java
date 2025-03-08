f7b3c291b869bff028475575e7c6dfe99d54ea59
Franco Armando <francoarmando1911@gmail.com>
2025-03-08 17:44:19 -0300

    Inicializacion ejercicio ppt

---
+++ src/main/java/retosProgMoureDev/PiedraPapelTijera.java
@@ -0,0 +1,19 @@
+/*
+ * Crea un programa que calcule quien gana más partidas al piedra,
+ * papel, tijera.
+ * - El resultado puede ser: "Player 1", "Player 2", "Tie" (empate)
+ * - La función recibe un listado que contiene pares, representando cada jugada.
+ * - El par puede contener combinaciones de "R" (piedra), "P" (papel)
+ *   o "S" (tijera).
+ * - Ejemplo. Entrada: [("R","S"), ("S","R"), ("P","S")]. Resultado: "Player 2".
+ */
+
+package retosProgMoureDev;
+
+public class PiedraPapelTijera {
+
+    public static void main(String[] args) {
+        
+    }
+    
+}
